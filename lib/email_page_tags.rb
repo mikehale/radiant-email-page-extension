6aa9c383359a5cb729c21609a2ff46fadb451a17
Michael Hale <mikehale@gmail.com>
2008-07-15 15:16:11 -0400

    remove debugger

--- lib/email_page_tags.rb
+++ lib/email_page_tags.rb
@@ -11,7 +11,8 @@ module EmailPageTags
   
   tag 'email_page:form' do |tag|
     subject = tag.attr["subject"]
-    %(<form id='email_page_form' action="/pages/#{tag.locals.page.page_id_to_email}/email_page" method='post'>
+    email_page = Page.find_by_title('Email Page')
+    %(<form id='email_page_form' action="/pages/#{email_page.page_id_to_email}/email_page" method='post'>
         <input type="hidden" name="subject" value="#{subject}"/>
         #{tag.expand}
       </form>)
